9377e15172cff045b161794a377a56db3c2f2ebd
Seifehab11-8 <160287704+Seifehab11-8@users.noreply.github.com>
2024-09-08 13:40:01 +0300

    Update school_database.c

--- school_database.c
+++ school_database.c
@@ -1,162 +1,437 @@
-#include<stdio.h>
-#include<stdint.h>
+/*
+ * project title: School Management system
+ * Date modified: 6 Aug 2024
+ * Author: Seif Ehab
+ */
 
-//creating a typedef structure to the student info.
-typedef struct 
+/* exceute in terminal gcc -o main main.c */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TRUE 1
+#define STRLENGTH 50
+
+typedef struct{
+	int id; // student's ID
+	char name[STRLENGTH]; // student's name
+	int age; // student's age
+	float gpa; // student's GPA
+}student;
+
+typedef struct node{
+	student data;
+	struct node *next; // should be defined using a struct name typedef is only accepted after the decleration is fully finished
+}node;
+
+/*Creating head and current pointers for student linked list*/
+node *head = NULL, *current = NULL; // Avoiding Wild pointers, assigning to NULL
+
+char *newline_pos = NULL; // used in getting rid of newlines during name scanning in the terminal
+
+void addStudent(const student *const ptr);
+void displayStudents(void);
+void searchStudentByID(int id);
+void updateStudent(int id);
+float calculateAverageGPA(void);
+void searchHighestGPA(void);
+void deleteStudent(int id);
+
+int main(void)
 {
-    int32_t ID;
-    char name[300];
-    char date_of_birth[20];
-    char major[100];
-    char semester[50];
-}school_database_t;
-/*==================================================*/
-void displayRecords(school_database_t*arr);
-void addNewRecords(school_database_t*arr);
-void deleteRecord (school_database_t* arr);
-void searchData (school_database_t* arr);
-/*==================================================*/
-//variable to store 10 student data
-school_database_t students[10];
+	int operation = 0, id = 0, termination_flag = 0;
+	float gpa_average = 0.0;
+	student student_temp;
 
-int main(){
-    uint32_t order;
-    while(1){
-        printf("Display all records -->1\n");
-        printf("Add new records     -->2\n");
-        printf("Delete a record     -->3\n");
-        printf("Search for st ID    -->4\n");
-        printf("Exit application    -->0\n");
-        printf("============================\nEnter your option here: ");
-        
-        if(!scanf("%d",&order)){
-            printf("You have entered an invalid input, Exiting...\n");
-            return 0;
-        }
-        if(order==1){
-            displayRecords(students);
-        }else if(order==2){
-            addNewRecords(students);
-        }else if(order==3){
-            deleteRecord(students);
-        }else if(order==4){
-            searchData(students);
-        }else if(!order){
-            printf("\nThank you for using the DATABASE\nExiting...\n");
-            return 0;
-        }else{
-            printf("You've entered a Wrong Option, Try Again...\n");
-        }
-    }
+	while(TRUE)
+	{
+		printf("---------------------------------------------\n");
+		printf("1. Add Student\n");
+		printf("2. Display Students\n");
+		printf("3. Search Student by ID\n");
+		printf("4. Update Student Information\n");
+		printf("5. Delete Student\n");
+		printf("6. Calculate Average GPA\n");
+		printf("7. Search for Student with Highest GPA\n");
+		printf("8. Exit\n");
+		printf("Enter choice: ");
+		scanf("%d", &operation);
+
+		switch(operation){
+		case 1:
+			/* scanning a new student */
+			printf("---------------------------------------------\n");
+
+			printf("Enter Student ID: ");
+			if(!scanf("%d", &(student_temp.id)))
+			{
+				printf("Something went wrong Exiting....\n");
+				return 0;
+			}
+			getchar();
+
+			printf("Enter name: "); fgets(student_temp.name, STRLENGTH, stdin);
+			newline_pos = strchr(student_temp.name, '\n'); // removes potential newlines caused by the fgets
+			if (newline_pos) {
+				*newline_pos = '\0';
+			}
+
+			printf("Enter age: ");
+			if(!scanf("%d", &(student_temp.age)))
+			{
+				printf("Something went wrong Exiting....\n");
+				return 0;
+			}
+
+			printf("Enter GPA: ");
+			if(!scanf("%f", &(student_temp.gpa)))
+			{
+				printf("Something went wrong Exiting....\n");
+				return 0;
+			}
+
+			addStudent(&student_temp);
+			/***********************************************************/
+			/* undefined error during scanning or during function call */
+			/***********************************************************/
+			break;
+		case 2:
+			displayStudents();
+			break;
+		case 3:
+			printf("Enter student ID to search: "); scanf("%d", &id);
+			searchStudentByID(id);
+			break;
+		case 4:
+			printf("Enter student ID to update: "); scanf("%d", &id);
+			updateStudent(id);
+			/***********************************************************/
+			/* undefined error during function call */
+			/***********************************************************/
+			break;
+		case 5:
+			printf("Enter student ID to remove the student's data: "); scanf("%d", &id);
+			deleteStudent(id);
+			break;
+		case 6:
+			gpa_average = calculateAverageGPA();
+			if(gpa_average == 0.0)
+			{
+				printf("---------------------------------------------\n");
+				printf("No students are found.\n");
+			}
+			else
+			{
+				printf("---------------------------------------------\n");
+				printf("The average GPA is [%0.2f].\n", gpa_average);
+			}
+			break;
+		case 7:
+			searchHighestGPA();
+			break;
+		case 8:
+			termination_flag = TRUE;
+			break;
+
+		}
+		/* To end the program */
+		if(termination_flag)
+		{
+			break;
+		}
+	}
 }
-void displayRecords(school_database_t*arr){
-    uint32_t flag=0;
-    for(uint32_t i=0 ;i<10; i++){
-        if(!((arr+i)->ID)){
-            flag++;
-            continue; //skip the iterations in which the user didn't input
-        }
-        printf("Student %d\n===============================\n",i+1);
-        printf("ID: %d\n",(arr+i)->ID);
-        printf("full name: %s\n",(arr+i)->name);
-        printf("Date of Birth: %s\n",(arr+i)->date_of_birth);
-        printf("Semester: %s\n",(arr+i)->semester);
-        printf("===============================\n");
+
+/*
+ * Description:
+ * It adds a new student to the linked list. It first checks if the student's ID already exists.
+ * If the ID exists, it prints an error message and returns. If the ID does not exist,
+ * it attempts to allocate memory for a new node.
+ * If memory allocation fails, it prints an error message and returns.
+ * If memory allocation is successful, it copies the student data to the new node
+ * and adds the node to the end of the linked list.
+ */
+
+void addStudent(const student *const ptr)
+{
+    node* link = NULL, *previous = NULL;
+
+    /* Checking if the id already exists */
+    current = head;
+    while(current != NULL)
+    {
+    	if(current ->data.id == ptr->id)
+    	{
+    		printf("---------------------------------------------\n");
+    		printf("Student with ID %d already exist.\n", ptr->id);
+    		return;
+    	}
+    	previous = current; // when current reaches null the previous will reach the last node
+    	current = current -> next;
     }
-    if(flag==10){
-        printf("==========================\n");
-        printf("The Database is Empty\n==========================\n");
+
+    link = (node*)malloc(sizeof(node)); //creating a new node should be sizeof(node) not sizeof(student) causes segementation fault error
+    current = head;
+
+    if(link == NULL)
+    {
+    	printf("---------------------------------------------\n");
+    	printf("Failed to save the data.\n");
+    	return;
+    }
+
+    /* Copying data to the node */
+    link ->data.id = ptr ->id;
+    strcpy(link ->data.name, ptr->name);
+    link ->data.age = ptr ->age;
+    link ->data.gpa = ptr ->gpa;
+
+    /* Attaching the node to the linked list */
+    link -> next = NULL;
+    if(head == NULL)
+    {
+        head = link;
+        return;
+    }
+
+    previous ->next = link;
+}
+
+/*
+ * Description:
+ * Displays all students in the linked list.
+ * If the list is empty, it prints a message indicating no students are present. Otherwise,
+ * it traverses the list and prints the details of each student.
+ */
+
+void displayStudents(void)
+{
+    current = head;
+    printf("---------------------------------------------\n");
+
+    if(head == NULL)
+    {
+    	printf("No students found.\n");
+    	return;
+    }
+
+    /* printing node by node */
+    while(current != NULL)
+    {
+        /* Displays the entire data of single student in one line*/
+    	printf("[ ID: %d, Name: %s, Age: %d, GPA: %0.2f ]\n",
+        		current ->data.id,
+        		current ->data.name,
+				current ->data.age,
+				current ->data.gpa
+				);
+
+        current = current ->next;
     }
 }
 
-void addNewRecords(school_database_t* arr){
-    uint32_t flag1=0,flag2=0;
-    for(uint32_t i=0 ;i<10;i++){
-        if(!((arr+i)->ID)){
-            printf("Enter student ID: ");
-            if(!scanf("%d",(&(arr[i]).ID))){
-                printf("you've entered invalid input\n");
-                return 0;
-            }
-            if(!((arr+i)->ID)){
-                printf("You have entered invalid ID\n");
-                ((arr+i)->ID)=0;
-                break;
-            }
-            for(uint32_t j=0; j<10;j++){
-                if(i==j){
-                    continue;
-                }
-                if(((arr+i)->ID)==((arr+j)->ID)){
-                    printf("These is a duplicate ID. INVALID\n");
-                    ((arr+i)->ID)=0;
-                    flag2++;
-                }
-            }
-            if(flag2){
-                break;
-            }
-            printf("Enter student full name: ");
-            getchar();// I dont know but seems to be a \n sended from somewhere that was causing errors
-            scanf("%[^\n]s",(arr+i)->name);
-            printf("Enter student birth date dd/mm/yy: ");
-            getchar();
-            scanf("%[^\n]s",(arr+i)->date_of_birth);
-            printf("Enter student Major: ");
-            getchar();
-            scanf("%[^\n]s",(arr+i)->major);
-            printf("Enter student Semester: ");
-            getchar();
-            scanf("%[^\n]s",(arr+i)->semester);
-            printf("===============================\n");
-            break;
+/*
+ * Description:
+ * It searches for a student by their ID in the linked list.
+ * If the student is found, it prints their details.
+ * If the student is not found, it prints a message indicating the student was not found.
+ */
+
+void searchStudentByID(int id)
+{
+    current = head;
+    while(current != NULL){
+        if(current -> data.id == id)
+        {
+        	printf("---------------------------------------------\n");
+        	printf("Student Found:\n");
+        	printf("[ ID: %d, Name: %s, Age: %d, GPA: %0.2f ]\n",
+					current ->data.id,
+					current ->data.name,
+					current ->data.age,
+					current ->data.gpa
+					);
+            return;
         }
-        flag1++;
+        current = current ->next;
     }
-    if(flag1==10){
-        printf("===============================\n");
-        printf("The Database is full please delete some users");
-        printf("===============================\n");
-    }
+    printf("---------------------------------------------\n");
+    printf("Student with ID %d not found.\n", id);
 }
-void deleteRecord (school_database_t* arr){
-    uint32_t IDsearch,flag=0;
-    printf("Enter student ID: ");
-    scanf("%d",&IDsearch);
-    for(uint32_t i=0;i<10;i++){
-        if((arr[i].ID)==IDsearch){
-            arr[i].ID=0;
-            printf("========================The user have been deleted Successfully\n");
-            printf("========================\n");
-            break;
-        }else{
-            flag++;
+
+/*
+ * Description:
+ * Updates the details of a student with a given ID in the linked list.
+ * If the student is found, then update the student's information (student name, age, gpa).
+ * If the student is not found, it prints a message indicating the student was not found.
+ */
+
+void updateStudent(int id)
+{
+    current = head;
+    char name[STRLENGTH];
+    while(current != NULL){
+    	/* Taking the new inputs from the user */
+        if(current ->data.id == id)
+        {
+        	printf("---------------------------------------------\n");
+        	printf("Enter new details of Student with ID %d:\n", id);
+
+        	getchar();
+
+            printf("Enter new name: ");
+            fgets(name, STRLENGTH, stdin); // checks for the length of string and stops the input once length -1 is reached
+
+            newline_pos = strchr(name, '\n'); // removes potential newlines caused by the fgets
+			if (newline_pos) {
+				*newline_pos = '\0';
+			}
+			strcpy(current ->data.name, name);
+
+            printf("Enter new age: ");
+            if(!scanf("%d", &(current ->data.age)))
+            {
+				printf("Something went wrong Exiting....\n");
+				return;
+			}
+
+            printf("Enter new GPA: ");
+            if(!scanf("%f", &(current ->data.gpa)))
+            {
+				printf("Something went wrong Exiting....\n");
+				return ;
+			}
+
+            printf("Student details updated successfully.\n");
+            return;
         }
+        current = current ->next;
     }
-    if(flag==10){
-        printf("There is no ID with the input you entered\n");
-    }
+
+    printf("---------------------------------------------\n");
+    printf("Student with ID %d not found.\n", id);
 }
-void searchData (school_database_t* arr){
-    uint32_t IDsearch,flag=0;
-    printf("Enter student ID: ");
-    scanf("%d",&IDsearch);
-    for(uint32_t i=0;i<10;i++){
-        if((arr[i].ID)==IDsearch){
-            printf("Student %d\n===============================\n",i+1);
-            printf("ID: %d\n",(arr+i)->ID);
-            printf("full name: %s\n",(arr+i)->name);
-            printf("Date of Birth: %s\n",(arr+i)->date_of_birth);
-            printf("Semester: %s\n",(arr+i)->semester);
-            printf("===============================\n");
-            break;
-        }else{
-            flag++;
+
+/*
+ * Description:
+ * Calculates and returns the average GPA of all students in the linked list.
+ * It traverses the list to sum the GPAs of all students and counts the number of students.
+ * If the list is empty, it returns 0.0.
+ * Otherwise, it computes and returns the average GPA by dividing the total sum of GPAs by the number of students.
+ */
+
+float calculateAverageGPA(void)
+{
+	float averageGPA = 0;
+    int count = 0;
+
+    if(head == NULL)
+    {
+    	return 0.0;// handling corner case which can cause the program to try and return 0/0 which will cause a runtime error
+    }
+
+	current = head;
+    while(current != NULL)
+    {
+        averageGPA += (current ->data.gpa);
+        count++;// count is incremented since the linked list size is unknown
+        current = current ->next;
+    }
+	averageGPA = averageGPA/count;
+
+	return averageGPA;
+}
+
+/*
+ * Description:
+ * Searches for the student with the highest GPA in the linked list.
+ * It traverses the list, keeping track of the student with the highest GPA encountered.
+ * If the list is empty, it prints a message indicating that there are no students.
+ * After finding the student with the highest GPA, it prints the details of that student.
+ */
+
+void searchHighestGPA(void)
+{
+    float maxGPA = 0;
+    node* maxPtr = head; // To stop at the student with the highest GPA
+
+    current = head;
+    if(head == NULL)
+    {
+    	printf("---------------------------------------------\n");
+        printf("No students found.\n");
+        return;
+    }
+
+    while(current != NULL)
+    {
+        if(current -> data.gpa > maxGPA)
+        {
+            maxGPA = current -> data.gpa;
+            maxPtr = current; // Updated when a new highest GPA is found
         }
+        current = current->next;
     }
-    if(flag==10){
-        printf("===============================\n");
-        printf("There is no ID with the input you Entered\n");
-        printf("===============================\n");
-    }
+
+    printf("---------------------------------------------\n");
+    printf("Student with the highest GPA is: \n");
+    printf("[ ID: %d, Name: %s, Age: %d, GPA: %0.2f ]\n",
+    		maxPtr ->data.id,
+			maxPtr ->data.name,
+			maxPtr ->data.age,
+			maxPtr ->data.gpa
+			);
 }
+
+/*
+ * Description:
+ * Iterates through the linked list of students starting with the Head of it to find the student with the given ID.
+ * If the student is found, it adjusts the pointers to remove the node from the list,
+ * frees the memory allocated for the node, and prints a success message.
+ * If the student is not found, it prints a message indicating the student was not found.
+ */
+
+void deleteStudent(int id)
+{
+    node* previous = head;
+    current = head;
+
+    if(head == NULL)
+    {
+    	printf("---------------------------------------------\n");
+    	printf("Students not found.\n");
+    	return;
+    }
+
+    /* if the data equal the head node data the head should be shifted */
+    if(head ->data.id == id)
+    {
+        head = head->next;
+        free(current); // freeing the heap memory after delinking the data
+        return;
+    }
+
+    /* searching for the data to be deleted*/
+    current = current -> next;
+    while(current != NULL)
+    {
+        if(current ->data.id == id)
+        {
+            previous ->next = current ->next;
+            free(current);// freeing the heap memory after delinking the data
+
+            printf("---------------------------------------------\n");
+            printf("Student with ID %d is removed successfully.\n", id);
+
+            return;
+        }
+        previous = current;
+        current = current ->next;
+    }
+
+    printf("---------------------------------------------\n");
+    printf("Student with ID %d not found.\n", id);
+}
+
+
